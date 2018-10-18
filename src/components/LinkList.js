c7ba9af177912d52302aff439d11fc0aa25d3442
Jimmy Hu <jimmyhu.co@gmail.com>
2018-10-18 13:20:24 +0800

    the declarative way for loading data with React & Apollo

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import Link from './Link'
 
-const linksToRender = [
+const FEED_QUERY = gql`
   {
-    id: '1',
-    description: 'Prisma turns your database into a GraphQL API 😎 😎',
-    url: 'https://www.prismagraphql.com'
-  },
-  {
-    id: '2',
-    description: 'The best GraphQL client',
-    url: 'https://www.apollographql.com/docs/react/'
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
   }
-]
+`
 
 const LinkList = () => {
   return (
-    <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>
+        if (error) return <div>Error</div>
+
+        const linksToRender = data.feed.links
+
+        return (
+          <div>
+            {linksToRender.map(link => <Link key={link.id} link={link} />)}
+          </div>
+        )
+      }}
+    </Query>
   )
 }
 
